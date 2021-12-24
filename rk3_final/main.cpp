1fff9446f4314c2b2a69b9660c69db1886fd9f18
Makar air <masemennikov@gmail.com>
2021-12-25 01:35:01 +0300

    Доделал РК3 осталось добавить проверки на ошибки и доработать интерфейс

--- rk3_final/main.cpp
+++ rk3_final/main.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 #define FILE_PATH "rk3_final/data.bin"
@@ -19,7 +20,6 @@
 typedef struct {
     char surname[20];
     char name[20];
-    char second_surname[20];
     char data[20];
     char first_exam[20];
     char second_exam[20];
@@ -48,6 +48,7 @@ int main() {
     int action = 0;
     int result = 0;
     Student student = {};
+    Student* students = NULL;
     while (true) {
         puts("Enter the action:");
         puts("\t1 Get all data from file");
@@ -60,13 +61,42 @@ int main() {
             case GET_ALL_DATA_FROM_FILE:
                 if (flag_file) {
                     fseek(fd, 0, SEEK_SET);
-                    while (fread(&student, sizeof(Student), 1, fd) != 1) {
+                    while (fread(&student, sizeof(Student), 1, fd) == 1) {
                         get_data(student);
                     }
                 }
                 break;
-            case GET_DATA_ABOUT_SOME_STUDENT:
+            case GET_DATA_ABOUT_SOME_STUDENT: {
+                fseek(fd, 0, SEEK_END);
+                long int pos = ftell(fd);
+                students = (Student *) malloc(pos);
+                fseek(fd, 0, SEEK_SET);
+                for (int i = 0; i < pos / sizeof(Student); ++i) {
+                    result = fread(&students[i], sizeof(students[i]), 1, fd);
+                    if (result != 1) {
+                        puts("Error of reading file");
+                    }
+                }
+                int index = 0;
+                for (int d = 1; d <= 31; ++d) {
+                    int flag_data = 0;
+                    for (int i = 0; i < pos / sizeof(Student); ++i) {
+                        if (atoi(students[i].data) == d && flag_data == 0) {
+                            flag_data = 1;
+                            index = i;
+                            puts("--------------");
+                        } else if (atoi(students[i].data) == d && flag_data == 1) {
+                            get_data(students[index]);
+                            get_data(students[i]);
+                            flag_data = 2;
+                        } else if (atoi(students[i].data) == d && flag_data == 2) {
+                            get_data(students[i]);
+                        }
+                    }
+                }
+                free(students);
                 break;
+            }
             case ADD_NEW_STUDENT_DATA:
                 if (flag_file) {
                     fseek(fd, 0, SEEK_END);
@@ -80,6 +110,9 @@ int main() {
                 }
                 break;
             case DELETE_ALL_DATA_IN_FILE:
+                if (flag_file == OPEN) {
+                    freopen(FILE_PATH, "w+b", fd);
+                }
                 break;
             case CLOSE_PROGRAM:
                 if (flag_file == OPEN) {
@@ -92,7 +125,7 @@ int main() {
 }
 
 void get_data(Student student) {
-    printf("%s %s %s %s %s %s %s\n",
+    printf("%s %s %s |%s| |%s| |%s| |%s|\n",
            student.surname,
            student.name,
            student.data,
@@ -107,20 +140,23 @@ Student add_data() {
     puts("Enter the:");
     puts("\tSurname");
     puts("\tName");
-    puts("\tSecond surname");
     puts("\tBirthday");
-    puts("\tFirst exam");
-    puts("\tSecond exam");
-    puts("\tThird exam");
-    puts("\tFourth exam");
-    scanf("%s %s %s %s %s %s %s %s",
-          st.surname,
+    scanf("%s %s %s",
           st.name,
-          st.second_surname,
-          st.data,
-          st.first_exam,
-          st.second_exam,
-          st.third_exam,
-          st.fourth_exam);
+          st.surname,
+          st.data);
+    puts("First exam");
+    while (getchar() != '\n');
+    fgets(st.first_exam, sizeof(st.first_exam), stdin);
+    *strchr(st.first_exam, '\n') = '\0';
+    puts("Second exam");
+    fgets(st.second_exam, sizeof(st.second_exam), stdin);
+    *strchr(st.second_exam, '\n') = '\0';
+    puts("Third exam");
+    fgets(st.third_exam, sizeof(st.third_exam), stdin);
+    *strchr(st.third_exam, '\n') = '\0';
+    puts("Fourth exam");
+    fgets(st.fourth_exam, sizeof(st.fourth_exam), stdin);
+    *strchr(st.fourth_exam, '\n') = '\0';
     return st;
 }
